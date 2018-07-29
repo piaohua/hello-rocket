597c350743057d15b9bcf9557fc380d70ee120a1
piaohua <twopiao7@gmail.com>
2018-07-29 22:36:02 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![feature(plugin, decl_macro)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
+}
