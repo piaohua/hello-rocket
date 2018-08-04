402ebadd5f24ea1b6f30d0c2e2c4132b68d7e91a
piaohua <twopiao7@gmail.com>
2018-08-04 22:18:32 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,40 @@
 #![feature(plugin, decl_macro)]
 #![plugin(rocket_codegen)]
 
+extern crate rocket_contrib;
 extern crate rocket;
 
+mod files;
+use rocket::Request;
+use rocket::response::Redirect;
+use rocket_contrib::{Template};
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Redirect {
+    Redirect::to("/hello/Unknown")
+}
+
+#[get("/hello/<name>")]
+fn hello(name: String) -> String {
+    name
+}
+
+#[catch(404)]
+fn not_found(req: &Request) -> Template {
+    let mut map = std::collections::HashMap::new();
+    map.insert("path", req.uri().as_str());
+    Template::render("error/404", &map)
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![
+               files::files, files::delete, files::down,
+               index, hello])
+        .attach(Template::fairing())
+        .catch(catchers![not_found])
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
 }
