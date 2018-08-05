76c13a5f3165fd520ec221d3fa02922394eaa049
piaohua <twopiao7@gmail.com>
2018-08-05 23:39:58 +0800

    update files

--- src/files.rs
+++ src/files.rs
@@ -1,7 +1,38 @@
+extern crate walkdir;
+
+use self::walkdir::{DirEntry, WalkDir};
+use std::io::Error;
+//use std::env;
+use std::path::Path;
 
 #[get("/files")]
 fn files() -> String {
+    let _ = file_list();
+    let _ = walk_count();
     format!("file list")
+}
+
+fn file_list() -> Result<(), Error> {
+    //for entry in WalkDir::new("static/files/") {
+    //    println!("{}", entry?.path().display());
+    //}
+    //
+    //for entry in WalkDir::new("static/files/").into_iter().filter_map(|e| e.ok()) {
+    //    println!("{}", entry.path().display());
+    //}
+    //
+    let walker = WalkDir::new("static/files/").into_iter();
+    for entry in walker.filter_entry(|e| !is_hidden(e)) {
+        println!("{}", entry?.path().display());
+    }
+    Ok(())
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry.file_name()
+         .to_str()
+         .map(|s| s.starts_with("."))
+         .unwrap_or(false)
 }
 
 #[get("/delete")]
@@ -17,4 +48,27 @@ fn download() -> &'static str {
 #[post("/upload")]
 fn upload() -> &'static str {
     "upload file"
+}
+
+fn walk<P: AsRef<Path>>(path: P) -> Result<(), Error> {
+    let mut count = 0;
+    for entry in WalkDir::new(path) {
+        let entry = entry?;
+        if entry.file_type().is_dir() {
+            continue;
+        }
+        let name = entry.file_name();
+        if let Some(s) = name.to_str() {
+            if s.to_ascii_lowercase().ends_with(".jpg") {
+                count += 1;
+            }
+        }
+    }
+    println!("{}", count);
+    Ok(())
+}
+
+fn walk_count() -> Result<(), Error> {
+    //walk(env::args().nth(1).unwrap())
+    walk("static/files/")
 }
